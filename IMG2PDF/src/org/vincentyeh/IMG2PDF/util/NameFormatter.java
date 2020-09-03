3778ef178d89e617e794b3fe53b6363680231753
VincentYeh-dev <vincent3314534@gmail.com>
2020-09-04 03:26:58 +0800

    NameFormatter is able to convet $PARENT{?} to parent name.

--- IMG2PDF/src/org/vincentyeh/IMG2PDF/util/NameFormatter.java
+++ IMG2PDF/src/org/vincentyeh/IMG2PDF/util/NameFormatter.java
@@ -2,36 +2,50 @@ package org.vincentyeh.IMG2PDF.util;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NameFormatter {
-	public NameFormatter(String format,File file) {
+	public NameFormatter(String format, File file) {
 		// TODO Auto-generated constructor stub
-		Matcher m = Pattern.compile(".*?\\$PARENT\\{([0-9]{1,})\\}").matcher(format); 
-		int level=-1;
-		String str_level=null;
-		if (m.matches()) 
-		{ 
-			str_level = m.group(1);
-			level=Integer.valueOf(str_level);
-		} 
+
+		String changed =format;
+		HashMap<String,Integer> ps=parentFormatter(format);
+		System.out.println(ps);
+		for(String key:ps.keySet()) {
+			int level=ps.get(key);
+			changed=changed.replace(key,getParentFile(file, level).getName());
+		}
+		
+		System.out.println("=======");
 		
 		Date date = new Date(file.lastModified());
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HHmmss");
 		String buf[] = file.getName().split("\\.");
 
-		String changed = format.replace("$NAME", buf[0]).replace("$DATE", sdf.format(date));
-		
-		if(level!=-1&&str_level!=null)
-			changed = changed.replace("$PARENT{"+str_level+"}", getParentFile(file, level).getAbsolutePath());
+		changed = changed.replace("$NAME", buf[0]).replace("$DATE", sdf.format(date));
 		System.out.println(changed);
+	}
+	HashMap<String,Integer> parentFormatter(String format){
+		Matcher matcher = Pattern.compile(".*?(\\$PARENT\\{[0-9]{1,}\\}).*?").matcher(format);
+		HashMap<String,Integer> map=new HashMap<String, Integer>();
+		while (matcher.find()) {
+			String parent=matcher.group(1);
+			Matcher matcher_num = Pattern.compile("\\$PARENT\\{([0-9]{1,})\\}").matcher(parent);
+			if(matcher_num.find()) {
+				int level=Integer.valueOf(matcher_num.group(1));
+				map.put(parent, level);
+			}
+		}
+		return map;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("NameFormatter Testing");
-		new NameFormatter("$PARENT{0}", new File("test_file\\"));
+		new NameFormatter("$PARENT{2}\\$PARENT{1}\\$PARENT{0}\\$NAME", new File("test_file\\"));
 	}
 
 	File getParentFile(File file, int index) {
