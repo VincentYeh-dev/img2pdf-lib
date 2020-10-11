fb4836f72625c2a5953643749ec76623415a4762
VincentYeh-dev <vincent3314534@gmail.com>
2020-10-12 01:49:38 +0800

    Change sample folder structure.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/commandline/QuicklyTester.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/commandline/QuicklyTester.java
@@ -9,11 +9,11 @@ public class QuicklyTester {
 				+ "-a CENTER|CENTER "
 				+ "-odr INCREASE "
 				+ "-lo test_file\\test.xml "
-				+ "-d D:\\Project\\git\\IMG2PDF\\sample\\$NAME.pdf "
+				+ "-d D:\\Project\\git\\IMG2PDF\\sample\\walk-animation\\output\\$PARENT{0}.pdf "
 //				+ "-usepwd 1234AAA "
 //				+ "-ownpwd 1234AAA "
 //				+ "test_file\\dirlist_mul.txt "
-				+ "D:\\Project\\git\\IMG2PDF\\sample\\sample-dirlist\\dirlist_walk.txt";
+				+ "D:\\Project\\git\\IMG2PDF\\sample\\walk-animation\\dirlist.txt";
 		
 //		TaskListCreator creator=new TaskListCreator(str);
 //		new TaskProcessor(creator.getTaskList());
