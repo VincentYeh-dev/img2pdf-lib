a800531ad5aefc29cb0e6065f1c0051e50298d69
VincentYeh-dev <vincent3314534@gmail.com>
2020-10-15 13:03:00 +0800

    Fix junit:junit security.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/commandline/QuicklyTester.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/commandline/QuicklyTester.java
@@ -4,6 +4,9 @@ public class QuicklyTester {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		new TaskListCreator("-h");
+		System.exit(0);
+		
 		String str="-sz A4 "
 				+ "-s NUMERTIC "
 				+ "-a CENTER|CENTER "
