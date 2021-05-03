0959e3aa22a0e0614c26b000e054a20a3bb86215
VincentYeh-dev <vincent3314534@gmail.com>
2021-05-04 01:40:20 +0800

    check language support

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/SharedSpace.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/SharedSpace.java
@@ -7,33 +7,52 @@ import java.util.ResourceBundle;
 
 
 public class SharedSpace {
-	public static class Configuration {
-		public static final String PROGRAM_NAME = "IMG2PDF";
-		public static final String PROGRAM_VER = "0.0.1-SNAPSHOT";
-		public static final String DEVELOPER = "VincentYeh-dev";
-		public static final Charset DEFAULT_CHARSET= StandardCharsets.UTF_8;
-	}
+    private static Locale[] supportedLocales = {
+            Locale.TRADITIONAL_CHINESE,
+            Locale.ENGLISH
+    };
 
-	private static ResourceBundle language_resource =null;
-	static {
-//		System.out.println("Static SharedSpace");
-//		Language Setting:
+    public static class Configuration {
+        public static final String PROGRAM_NAME = "IMG2PDF";
+        public static final String PROGRAM_VER = "0.0.1-SNAPSHOT";
+        public static final String DEVELOPER = "VincentYeh-dev";
+        public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+    }
 
-//	Local:
-		SharedSpace.setLanguageRes(ResourceBundle.getBundle("language_package", Locale.getDefault()));
-//	root:
+    private static Locale locale;
+    private static ResourceBundle language_resource = null;
 
-//		Configuration.setLanguageRes(ResourceBundle.getBundle("language_package", Locale.ROOT));
-	}
+    static {
+        setLocale(Locale.getDefault());
+        updateLanguageResource();
+    }
 
-	public static void setLanguageRes(ResourceBundle lagug_resource) {
-		SharedSpace.language_resource = lagug_resource;
-	}
+    public static void setLocale(Locale locale) {
+        SharedSpace.locale = locale;
+    }
 
-	
-	public static String getResString(String key) {
-		return SharedSpace.language_resource.getString(key);
-	}
+    public static void updateLanguageResource() {
+        SharedSpace.setLanguageRes(ResourceBundle.getBundle("language_package", getLanguageSupport(locale)));
+
+    }
+
+    private static Locale getLanguageSupport(Locale target){
+        for(Locale locale:supportedLocales){
+            if(target.equals(locale)){
+                return locale;
+            }
+        }
+        return Locale.ROOT;
+    }
+
+    public static void setLanguageRes(ResourceBundle LanguageRes) {
+        SharedSpace.language_resource = LanguageRes;
+    }
+
+
+    public static String getResString(String key) {
+        return SharedSpace.language_resource.getString(key);
+    }
 
 
 }
