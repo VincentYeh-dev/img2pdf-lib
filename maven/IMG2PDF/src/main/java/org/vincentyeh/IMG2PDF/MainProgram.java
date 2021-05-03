073d4ce59ab3831a0c39997bb48e6b2ac0f60fcd
VincentYeh-dev <vincent3314534@gmail.com>
2021-05-04 01:16:35 +0800

    Handle Exception in MainProgram

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/MainProgram.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/MainProgram.java
@@ -9,36 +9,45 @@ import org.vincentyeh.IMG2PDF.commandline.parser.MainActionParser;
 import org.vincentyeh.IMG2PDF.commandline.parser.core.HandledException;
 import org.vincentyeh.IMG2PDF.commandline.parser.exception.HelperException;
 
-import java.util.Iterator;
+import java.util.stream.Collectors;
 
 public class MainProgram {
+    private final String[] args;
+
+    private MainProgram(String[] args){
+        this.args = args;
+    }
+
     public static void main(String[] args) {
+        MainProgram main=new MainProgram(args);
+        try {
+            main.start();
+        }catch (HandledException e){
+           System.err.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void start() throws Exception {
         MainActionParser parser = new MainActionParser();
         try {
             MainAction action = parser.parse(args);
             action.start();
         } catch (HelperException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp(SharedSpace.Configuration.PROGRAM_NAME, e.getOptions());
+            throw new HandledException(e,getClass());
         } catch (MissingOptionException e) {
-            StringBuilder buf = new StringBuilder();
-            Iterator<?> it = e.getMissingOptions().iterator();
-            while (it.hasNext()) {
-                buf.append(it.next());
-                if (it.hasNext()) {
-                    buf.append(", ");
-                }
-            }
-            System.err.printf(SharedSpace.getResString("argperser.err.missing_option") + "\n", buf);
+            System.err.printf(SharedSpace.getResString("argperser.err.missing_option") + "\n", e.getMissingOptions().stream().map(Object::toString).collect(Collectors.joining(",")));
+            throw new HandledException(e,getClass());
         } catch (MissingArgumentException e) {
             System.err.printf(SharedSpace.getResString("argperser.err.missing_argument_option") + "\n", e.getOption().getOpt());
+            throw new HandledException(e,getClass());
         } catch (UnrecognizedOptionException e) {
             System.err.printf(SharedSpace.getResString("public.err.unrecognized_argument_option") + "\n", e.getOption());
-        }catch (HandledException e) {
-            System.err.println(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
+            throw new HandledException(e,getClass());
         }
     }
 }
