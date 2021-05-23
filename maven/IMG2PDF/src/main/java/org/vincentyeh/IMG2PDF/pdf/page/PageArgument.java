4d65368c7108c597be480b03b1ea4d16204335fc
VincentYeh-dev <vincent3314534@gmail.com>
2021-05-23 23:40:25 +0800

    Add Builder in PageArgument

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/pdf/page/PageArgument.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/pdf/page/PageArgument.java
@@ -4,46 +4,57 @@ import org.jdom2.Element;
 
 
 public class PageArgument {
-    private PageAlign align=new PageAlign("CENTER-CENTER");
-    private PageSize size=PageSize.A4;
-    private PageDirection direction=PageDirection.Portrait;
-    private boolean auto_rotate=true;
 
-    public PageArgument(){
 
+    public static class Builder {
+        private PageAlign align;
+        private PageSize size;
+        private PageDirection direction;
+        private boolean auto_rotate;
+
+        public Builder setAlign(PageAlign align) {
+            this.align = align;
+            return this;
+        }
+
+        public Builder setSize(PageSize size) {
+            this.size = size;
+            return this;
+        }
+
+        public Builder setDirection(PageDirection direction) {
+            this.direction = direction;
+            return this;
+        }
+
+        public Builder setAutoRotate(boolean auto_rotate) {
+            this.auto_rotate = auto_rotate;
+            return this;
+        }
+
+        public PageArgument build() {
+            return new PageArgument(align, size, direction, auto_rotate);
+        }
+
+        public PageArgument buildFrom(Element element) {
+            setAlign(new PageAlign(element.getChild("align").getValue()));
+            setSize(PageSize.valueOf(element.getChild("size").getValue()));
+            setDirection(PageDirection.valueOf(element.getChild("default-direction").getValue()));
+            setAutoRotate(Boolean.parseBoolean(element.getChild("auto-rotate").getValue()));
+            return build();
+        }
     }
 
-    public PageArgument(Element element){
-        this.align = new PageAlign(element.getChild("align").getValue());
-        this.size = PageSize.valueOf(element.getChild("size").getValue());
-        this.direction = PageDirection.valueOf(element.getChild("default-direction").getValue());
-        this.auto_rotate = Boolean.parseBoolean(element.getChild("auto-rotate").getValue());
-    }
+    private final PageAlign align;
+    private final PageSize size;
+    private final PageDirection direction;
+    private final boolean auto_rotate;
 
-    public Element toElement(){
-        Element element = new Element("PageArgument");
-        element.addContent(new Element("size").addContent(size.toString()));
-        element.addContent(new Element("align").addContent(align.toString()));
-        element.addContent(new Element("default-direction").addContent(direction.toString()));
-        element.addContent(new Element("auto-rotate").addContent(Boolean.toString(auto_rotate)));
-        return element;
-
-    }
-
-    public void setAlign(PageAlign align) {
+    private PageArgument(PageAlign align, PageSize size, PageDirection direction, boolean auto_rotate) {
         this.align = align;
-    }
-
-    public void setAutoRotate(boolean auto_rotate) {
-        this.auto_rotate = auto_rotate;
-    }
-
-    public void setDirection(PageDirection direction) {
-        this.direction = direction;
-    }
-
-    public void setSize(PageSize size) {
         this.size = size;
+        this.direction = direction;
+        this.auto_rotate = auto_rotate;
     }
 
     public PageAlign getAlign() {
@@ -58,8 +69,18 @@ public class PageArgument {
         return size;
     }
 
-    public boolean getAutoRotate(){
+    public boolean getAutoRotate() {
         return auto_rotate;
+    }
+
+    public Element toElement() {
+        Element element = new Element("PageArgument");
+        element.addContent(new Element("size").addContent(size.toString()));
+        element.addContent(new Element("align").addContent(align.toString()));
+        element.addContent(new Element("default-direction").addContent(direction.toString()));
+        element.addContent(new Element("auto-rotate").addContent(Boolean.toString(auto_rotate)));
+        return element;
+
     }
 
 }
