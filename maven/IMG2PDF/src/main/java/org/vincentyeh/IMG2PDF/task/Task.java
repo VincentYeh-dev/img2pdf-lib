24a72c27fb935dce60f4a8c69d899bb960c5145e
VincentYeh-dev <vincent3314534@gmail.com>
2021-05-27 00:57:02 +0800

    Replace the loop in parseElementsToImages to Iterator.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/task/Task.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/task/Task.java
@@ -1,14 +1,12 @@
 package org.vincentyeh.IMG2PDF.task;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jdom2.Element;
 import org.vincentyeh.IMG2PDF.pdf.doc.DocumentArgument;
 import org.vincentyeh.IMG2PDF.pdf.page.PageArgument;
-import org.vincentyeh.IMG2PDF.util.file.FileUtils;
 
 public class Task {
     private final DocumentArgument documentArgument;
@@ -18,7 +16,7 @@ public class Task {
     private final File destination;
 
 
-    public Task(DocumentArgument documentArgument, PageArgument pageArgument, File[] images,File destination) {
+    public Task(DocumentArgument documentArgument, PageArgument pageArgument, File[] images, File destination) {
         if (images == null)
             throw new IllegalArgumentException("images is null");
         this.images = images;
@@ -27,20 +25,20 @@ public class Task {
         this.pageArgument = pageArgument;
     }
 
-    public Task(Element element) throws FileNotFoundException,IllegalArgumentException{
+    public Task(Element element) throws IllegalArgumentException {
         if (element == null)
             throw new NullPointerException("element is null");
 
         this.documentArgument = new DocumentArgument.Builder().buildFrom(element.getChild("DocumentArgument"));
         this.pageArgument = new PageArgument.Builder().buildFrom((element.getChild("PageArgument")));
-        this.destination=new File(element.getChild("destination").getValue());
+        this.destination = new File(element.getChild("destination").getValue());
         List<Element> contains_files = element.getChild("files").getChildren("image");
         ArrayList<Element> xml_files = new ArrayList<>(contains_files);
         this.images = parseElementsToImages(xml_files);
 
     }
 
-   public Element toElement() {
+    public Element toElement() {
         Element task = new Element("task");
         Element xml_files = new Element("files");
         for (File img : images) {
@@ -49,7 +47,7 @@ public class Task {
             xml_files.addContent(xml_file);
         }
         task.addContent(xml_files);
-        Element elm_destination=new Element("destination");
+        Element elm_destination = new Element("destination");
         elm_destination.addContent(destination.getAbsolutePath());
 
         task.addContent(elm_destination);
@@ -59,18 +57,10 @@ public class Task {
         return task;
     }
 
-    private File[] parseElementsToImages(ArrayList<Element> el_files) throws FileNotFoundException {
-        File[] imgFiles = new File[el_files.size()];
-        for (int i = 0; i < imgFiles.length; i++) {
-            Element el = el_files.get(i);
-            imgFiles[i] = new File(new File(el.getValue()).getAbsolutePath());
-            FileUtils.checkExists(imgFiles[i]);
-            if (imgFiles[i].isDirectory())
-                throw new RuntimeException(imgFiles[i].getAbsolutePath()+" is directory.");
+    private File[] parseElementsToImages(ArrayList<Element> elements) {
 
-        }
+        return elements.stream().map(element -> new File(element.getValue())).toArray(File[]::new);
 
-        return imgFiles;
     }
 
 
@@ -89,7 +79,6 @@ public class Task {
     public PageArgument getPageArgument() {
         return pageArgument;
     }
-
 
 
 }
--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/task/TaskList.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/task/TaskList.java
@@ -1,21 +1,20 @@
 package org.vincentyeh.IMG2PDF.task;
 
-import java.io.*;
 import java.util.ArrayList;
+
 import org.jdom2.Document;
 import org.jdom2.Element;
 
-public class TaskList extends ArrayList<Task>{
-//    private final ArrayList<Task> arrayList = new ArrayList<>();
+public class TaskList extends ArrayList<Task> {
 
     public TaskList() {
     }
 
-    public TaskList(Document doc) throws FileNotFoundException, IllegalArgumentException {
+    public TaskList(Document doc) throws IllegalArgumentException {
         this(doc.getRootElement());
     }
 
-    private TaskList(Element root) throws FileNotFoundException, IllegalArgumentException {
+    private TaskList(Element root) throws IllegalArgumentException {
         ArrayList<Element> importedTaskList = new ArrayList<>(root.getChildren("task"));
         for (Element task : importedTaskList) {
             this.add(new Task(task));
