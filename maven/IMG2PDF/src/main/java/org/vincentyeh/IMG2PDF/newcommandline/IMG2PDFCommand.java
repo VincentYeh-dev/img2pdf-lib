5c1d00a3c3c327adcd90d2ed4c894b5e387db0ec
VincentYeh-dev <vincent3314534@gmail.com>
2021-06-06 01:30:01 +0800

    Add help/version options.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/newcommandline/IMG2PDFCommand.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/newcommandline/IMG2PDFCommand.java
@@ -4,13 +4,18 @@ import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "img2pdf",subcommands = CreateCommand.class,resourceBundle = "cmd")
+@CommandLine.Command(name = "img2pdf", mixinStandardHelpOptions = true,
+        version = {
+        "IMG2PDF-0.0.1-SNAPSHOT",
+        "Picocli " + picocli.CommandLine.VERSION,
+        "JVM: ${java.version} (${java.vendor} ${java.vm.name} ${java.vm.version})",
+        "OS: ${os.name} ${os.version} ${os.arch}"},
+        subcommands = CreateCommand.class, resourceBundle = "cmd")
+
 public class IMG2PDFCommand implements Callable<Integer> {
-    @CommandLine.Option(names = {"--debug","-d"})
+    @CommandLine.Option(names = {"--debug", "-d"})
     boolean debug;
 
-    @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true)
-    boolean usageHelpRequested;
 
     @Override
     public Integer call() throws Exception {
