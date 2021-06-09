65c2adb4ec70e63d34cad7c0f80e20ffb4ca6e2f
VincentYeh-dev <vincent3314534@gmail.com>
2021-06-09 15:51:59 +0800

    Fix uncheck cast in HandlerRegister.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/pattern/HandlerRegister.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/pattern/HandlerRegister.java
@@ -1,19 +1,20 @@
 package org.vincentyeh.IMG2PDF.pattern;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class HandlerRegister<T,R> {
+public class HandlerRegister<T, R> {
     private final List<Class<? extends Handler<T, R>>> handlerClasses = new ArrayList<>();
 
     public void registerHandler(Class<? extends Handler<T, R>> clazz) {
         handlerClasses.add(clazz);
     }
 
-    public Handler<T,R> getHandler() throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        List<Constructor<?>> constructors = handlerClasses.stream()
+    public Handler<T, R> getHandler() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        List<Constructor<? extends Handler<T, R>>> constructors = handlerClasses.stream()
                 .map(c -> {
                     try {
                         return c.getConstructor(Handler.class);
@@ -23,9 +24,9 @@ public class HandlerRegister<T,R> {
                     }
                 }).collect(Collectors.toList());
 
-        Handler<T,R> last = null;
-        for (Constructor<?> constructor : constructors) {
-            last = (Handler<T,R>) constructor.newInstance(last);
+        Handler<T, R> last = null;
+        for (Constructor<? extends Handler<T, R>> constructor : constructors) {
+            last = constructor.newInstance(last);
         }
 
         return last;
