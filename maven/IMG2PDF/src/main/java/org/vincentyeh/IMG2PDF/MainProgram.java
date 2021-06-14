b2a1f2a16847c81325a3b1105ddabc582f3dd89d
VincentYeh-dev <vincent3314534@gmail.com>
2021-06-15 02:35:44 +0800

    Add JANSI dependency.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/MainProgram.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/MainProgram.java
@@ -1,5 +1,6 @@
 package org.vincentyeh.IMG2PDF;
 
+import org.fusesource.jansi.AnsiConsole;
 import org.vincentyeh.IMG2PDF.commandline.command.IMG2PDFCommand;
 import org.vincentyeh.IMG2PDF.commandline.command.ConvertCommand;
 import org.vincentyeh.IMG2PDF.commandline.handler.ExecutionHandler;
@@ -12,6 +13,7 @@ import java.io.*;
 public class MainProgram {
 
     public static void main(String[] args) {
+        AnsiConsole.systemInstall();
         ConfigurationAgent.loadOrCreateProperties(new File("config.properties"));
 
         CommandLine cmd = new CommandLine(new IMG2PDFCommand());
@@ -21,6 +23,8 @@ public class MainProgram {
         cmd.setParameterExceptionHandler(new ParameterHandler(ConfigurationAgent.getHandlerResourceBundle()));
         cmd.setResourceBundle(ConfigurationAgent.getCommandResourceBundle());
 
-        System.exit(cmd.execute(args));
+        int exitCode=cmd.execute(args);
+        AnsiConsole.systemUninstall();
+        System.exit(exitCode);
     }
 }
