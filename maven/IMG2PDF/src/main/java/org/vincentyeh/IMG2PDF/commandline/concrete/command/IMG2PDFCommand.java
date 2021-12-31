e91b96031ddbbe24b191ecd817c280a2397d130a
VincentYeh-dev <vincent3314534@gmail.com>
2022-01-01 01:19:56 +0800

    Add Constants to declare app name and version.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/commandline/concrete/command/IMG2PDFCommand.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/commandline/concrete/command/IMG2PDFCommand.java
@@ -1,18 +1,18 @@
 package org.vincentyeh.IMG2PDF.commandline.concrete.command;
 
+import org.vincentyeh.IMG2PDF.Constants;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "img2pdf", mixinStandardHelpOptions = true,
         version = {
-        "IMG2PDF v1.0.0",
-        "Picocli " + picocli.CommandLine.VERSION,
-        "JVM: ${java.version} (${java.vendor} ${java.vm.name} ${java.vm.version})",
-        "OS: ${os.name} ${os.version} ${os.arch}"}
-        )
-
+                Constants.APP_NAME + " " + Constants.APP_VERSION,
+                "Picocli " + picocli.CommandLine.VERSION,
+                "JVM: ${java.version} (${java.vendor} ${java.vm.name} ${java.vm.version})",
+                "OS: ${os.name} ${os.version} ${os.arch}"}
+)
 public class IMG2PDFCommand {
 
-    @CommandLine.Option(names = {"--debug","-d"})
+    @CommandLine.Option(names = {"--debug", "-d"})
     private boolean debug;
 
     public boolean isDebug() {
