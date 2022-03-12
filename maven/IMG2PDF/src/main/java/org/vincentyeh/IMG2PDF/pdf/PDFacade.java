c36748842773a1c1fb07d29c26a3a93a1f37302e
VincentYeh-dev <vincent3314534>
2022-03-12 12:01:07 +0800

    Apply bridge pattern to ImagePDFCreator.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/pdf/PDFacade.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/pdf/PDFacade.java
@@ -4,10 +4,12 @@ import org.vincentyeh.IMG2PDF.image.helper.concrete.ColorSpaceImageHelper;
 import org.vincentyeh.IMG2PDF.image.helper.concrete.DirectionImageHelper;
 import org.vincentyeh.IMG2PDF.pdf.concrete.appender.ExecutorPageAppender;
 import org.vincentyeh.IMG2PDF.pdf.concrete.calculation.strategy.StandardImagePageCalculationStrategy;
+import org.vincentyeh.IMG2PDF.pdf.concrete.converter.ImageHelperPDFCreatorImpl;
 import org.vincentyeh.IMG2PDF.pdf.concrete.converter.PDFBoxCreatorImpl;
 import org.vincentyeh.IMG2PDF.pdf.concrete.listener.ProgressBarCreationListener;
 import org.vincentyeh.IMG2PDF.pdf.framework.converter.PDFCreator;
 import org.vincentyeh.IMG2PDF.pdf.function.converter.ImagePDFCreator;
+import org.vincentyeh.IMG2PDF.pdf.function.converter.ImagePDFCreatorImpl;
 import org.vincentyeh.IMG2PDF.util.file.exception.MakeDirectoryException;
 
 import java.awt.color.ColorSpace;
@@ -20,9 +22,11 @@ public class PDFacade {
     }
 
     public static PDFCreator createImagePDFConverter(long bytes_count, File tempFolder, boolean overwrite_output, Locale locale, ColorSpace colorSpace, int nThread) throws MakeDirectoryException {
-        PDFBoxCreatorImpl impl = new PDFBoxCreatorImpl(tempFolder, bytes_count);
+        PDFBoxCreatorImpl pdfBoxCreatorImpl = new PDFBoxCreatorImpl(tempFolder, bytes_count);
+        ImagePDFCreatorImpl imagePDFCreatorImpl = new ImageHelperPDFCreatorImpl(new DirectionImageHelper(new ColorSpaceImageHelper(colorSpace)));
         ExecutorPageAppender appender = new ExecutorPageAppender(nThread);
-        PDFCreator converter = new ImagePDFCreator(impl, appender, overwrite_output, new StandardImagePageCalculationStrategy(), new DirectionImageHelper(new ColorSpaceImageHelper(colorSpace)));
+
+        PDFCreator converter = new ImagePDFCreator(pdfBoxCreatorImpl,imagePDFCreatorImpl, appender, overwrite_output, new StandardImagePageCalculationStrategy());
 
         ProgressBarCreationListener listener = new ProgressBarCreationListener(locale);
         converter.setCreationListener(listener);
--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/pdf/function/converter/ImagePDFCreator.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/pdf/function/converter/ImagePDFCreator.java
@@ -1,15 +1,12 @@
 package org.vincentyeh.IMG2PDF.pdf.function.converter;
 
-import org.vincentyeh.IMG2PDF.image.helper.framework.ImageHelper;
 import org.vincentyeh.IMG2PDF.pdf.framework.appender.PageAppender;
 import org.vincentyeh.IMG2PDF.pdf.framework.calculation.strategy.ImagePageCalculateStrategy;
 import org.vincentyeh.IMG2PDF.pdf.framework.converter.PDFCreator;
 import org.vincentyeh.IMG2PDF.pdf.framework.converter.PDFCreatorImpl;
-import org.vincentyeh.IMG2PDF.pdf.framework.converter.exception.ReadImageException;
 import org.vincentyeh.IMG2PDF.pdf.framework.objects.PdfDocument;
 import org.vincentyeh.IMG2PDF.pdf.framework.objects.PdfPage;
 import org.vincentyeh.IMG2PDF.task.framework.Task;
-import org.vincentyeh.IMG2PDF.util.file.FileUtils;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -18,13 +15,13 @@ import java.util.List;
 import java.util.concurrent.Callable;
 
 public class ImagePDFCreator extends PDFCreator {
+    private final ImagePDFCreatorImpl imagePDFCreatorImpl;
     private final ImagePageCalculateStrategy strategy;
-    private final ImageHelper helper;
 
-    public ImagePDFCreator(PDFCreatorImpl impl, PageAppender pageAppender, boolean overwrite, ImagePageCalculateStrategy strategy, ImageHelper helper) {
-        super(impl, pageAppender, overwrite);
+    public ImagePDFCreator(PDFCreatorImpl pdfCreatorImpl, ImagePDFCreatorImpl imagePDFCreatorImpl, PageAppender pageAppender, boolean overwrite, ImagePageCalculateStrategy strategy) {
+        super(pdfCreatorImpl, pageAppender, overwrite);
+        this.imagePDFCreatorImpl = imagePDFCreatorImpl;
         this.strategy = strategy;
-        this.helper = helper;
     }
 
     @Override
@@ -34,7 +31,7 @@ public class ImagePDFCreator extends PDFCreator {
         for (File file : files) {
             list.add(() -> {
                 PdfPage<?> page = impl.createEmptyPage(document);
-                BufferedImage bufferedImage = readImage(file);
+                BufferedImage bufferedImage = imagePDFCreatorImpl.readImage(file);
                 strategy.execute(task.getPageArgument(), bufferedImage);
                 page.setSize(strategy.getPageSize());
                 page.putImage(bufferedImage, strategy.getImagePosition(), strategy.getImageSize());
@@ -44,16 +41,4 @@ public class ImagePDFCreator extends PDFCreator {
         return list;
     }
 
-
-    private BufferedImage readImage(File file) throws ReadImageException {
-        try {
-            FileUtils.checkExists(file);
-            BufferedImage image = helper.read(file);
-            if (image == null)
-                throw new RuntimeException("image==null");
-            return image;
-        } catch (Exception e) {
-            throw new ReadImageException(e, file);
-        }
-    }
 }
