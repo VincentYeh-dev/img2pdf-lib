11d7abbb750a57843d02c9497a7480f570566326
vincent <vincent@DESKTOP-FPPVIOM>
2025-04-13 02:44:30 +0800

    更改package path

---
+++ src/main/java/org/vincentyeh/img2pdf/lib/pdf/parameter/PageDirection.java
@@ -0,0 +1,21 @@
+package org.vincentyeh.img2pdf.lib.pdf.parameter;
+
+
+import org.vincentyeh.img2pdf.lib.pdf.framework.objects.SizeF;
+
+/**
+ * Direction of page.Horizontal or Vertical.
+ *
+ * @author VincentYeh
+ */
+public enum PageDirection {
+    Landscape, Portrait;
+
+    public static PageDirection detectDirection(SizeF size) {
+        return detectDirection(size.width, size.height);
+    }
+
+    public static PageDirection detectDirection(float width, float height) {
+        return height / width > 1 ? Portrait : Landscape;
+    }
+}
