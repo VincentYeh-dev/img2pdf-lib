09b1135ee9b83e673a386a5236654b0a3997f5f4
vince <vincent11515@gmail.com>
2026-03-10 13:34:24 +0800

    新增 IDocument 向後相容過渡方法（save/close @Deprecat…)

--- src/main/java/org/vincentyeh/img2pdf/lib/pdf/framework/factory/IDocument.java
+++ src/main/java/org/vincentyeh/img2pdf/lib/pdf/framework/factory/IDocument.java
@@ -11,4 +11,24 @@ public interface IDocument {
     void saveAndClose(File destination) throws IOException;
 
     int getPageCount();
+
+    /** @deprecated Use {@link #saveAndClose(OutputStream)} instead. */
+    @Deprecated
+    default void save(OutputStream outputStream) throws IOException {
+        saveAndClose(outputStream);
+    }
+
+    /** @deprecated Use {@link #saveAndClose(File)} instead. */
+    @Deprecated
+    default void save(File destination) throws IOException {
+        saveAndClose(destination);
+    }
+
+    /**
+     * @deprecated Resources are released by saveAndClose(). This is a no-op for backward compatibility.
+     */
+    @Deprecated
+    default void close() {
+        // no-op：saveAndClose 已在內部完成資源釋放
+    }
 }
